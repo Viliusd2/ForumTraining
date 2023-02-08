3b11cb3766689da83fa98b3fce2004436040f387
Vilius <viliuxasssd@gmail.com>
2023-02-08 04:41:36 +0000

    1 New Training project

---
+++ src/main/java/com/training/forumexample/ForumExampleApplication.java
@@ -0,0 +1,13 @@
+package com.training.forumexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/forumexample/ForumExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.forumexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
